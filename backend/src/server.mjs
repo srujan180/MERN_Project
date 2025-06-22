f044dd5f64defc4470e75ec79153dc01429313b2
Srujan180 <annamsrujanreddy2002@gmail.com>
2025-06-22 15:21:15 +0530

    Moved files

--- backend/src/server.mjs
+++ backend/src/server.mjs
@@ -4,11 +4,13 @@ import {connectDB} from "../config/db.js"
 import notesRoutes from "../src/routes/notesRoutes.js";
 import rateLimit from "../config/upstash.js";
 import rateLimiter from "../config/ratelimit.js";
+import cors from "cors"
 dotenv.config();
 
 const app = express() 
 connectDB();
 app.use(express.json());
+app.use(cors())
 app.use(rateLimiter);
 app.use("/api/notes", notesRoutes)
 app.listen(5001,()=>{
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,19 @@
-import React from 'react'
-import {Route , Routes} from "react-router"
-import HomePage from '../pages/HomePage.jsx'
-import NoteDetailPage from '../pages/NoteDetailPage.jsx'
-import CreatePage from '../pages/CreatePage.jsx'
-import toast from "react-hot-toast"
+import { Route, Routes } from "react-router";
+
+import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
+import NoteDetailPage from "./pages/NoteDetailPage";
+
 const App = () => {
   return (
-    <div>
-      <button onClick={()=>toast.success("Congrats")}className='text-red'>Success</button>
+    <div className="relative h-full w-full">
+      <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_60%,#00FF9D40_100%)]" />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/create" element={<CreatePage/>}/>
-        <Route path="/note/:id" element={<NoteDetailPage/>}/>
-        </Routes>
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/note/:id" element={<NoteDetailPage />} />
+      </Routes>
     </div>
-  )
-}
-
+  );
+};
 export default App;
