c15ef1b2b71e92abca8982ca45baf8212324d7b2
Srujan Annam <108586853+srujan180@users.noreply.github.com>
2025-06-24 23:24:44 +0530

    Update server.mjs

--- backend/src/server.mjs
+++ backend/src/server.mjs
@@ -32,7 +32,7 @@ app.use("/api/notes", notesRoutes);
 
 // Serve frontend in production
 if (process.env.NODE_ENV === "production") {
-  const distPath = path.resolve(__dirname, "../../../frontend/dist");
+  const distPath = path.resolve(__dirname, "../frontend/dist");
 
   app.use(express.static(distPath));
 
